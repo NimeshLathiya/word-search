30339b89e308f0ede73efb41dbb56cf3b0f907c0
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-11-12 09:03:59 +0530

    Update Definitions.js

--- src/Components/Definitions/Definitions.js
+++ src/Components/Definitions/Definitions.js
@@ -3,7 +3,7 @@ import "./Definitions.css";
 
 const Definitions = ({ word, meanings, catagery, lightMode }) => {
   return (
-    <div className="meanings">
+    <div 
       {/*ANCHOR audio---------------------------- */}
       {meanings[0] && word && catagery === "en" && (
         <audio
